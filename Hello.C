7b8af08df9e2c6a65e96863d34bf19fe8758060e
Grant Baumhover <42647879+GrantBaumhover@users.noreply.github.com>
2018-08-23 10:33:16 -0500

    Hello.C

---
+++ Hello.C
@@ -0,0 +1,16 @@
+/**
+ * Author: Grant Baumhover
+ * Date: 2018/8/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Grant Baumhover Software Engineer!\n");
+
+  return 0;
+}
